235d1bae1c287e7a289a2ad760e537adaecc2730
Ian Ramirez España <ianespana@hotmail.com>
2021-03-20 13:36:55 -0600

    Update docker commands

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-
 rm -f /var/run/apache2/apache2.pid
+source /etc/apache2/envvars
+pg_ctlcluster 12 main start
+redis-server --daemonize yes
+RAILS_ENV=production bundle exec rake db:initial_setup
 /usr/sbin/apache2ctl -D FOREGROUND -e info
