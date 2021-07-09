2d8a4dc447beceb71deb2bfc4c9cf84eac57af9e
Ian Ramirez España <ianespana@hotmail.com>
2021-07-09 13:17:01 -0500

    Reformatted

--- start.sh
+++ start.sh
@@ -6,6 +6,7 @@ line=$(head -n 1 assert.tmp)
 if ! [ $line -ge 2 ]; then
   RAILS_ENV=production bundle exec rake db:initial_setup
 fi
+
 rm assert.tmp
 /etc/init.d/canvas_init start
 echo "canvasuser:${EMAIL_OUTGOING_ADDRESS}" > /etc/ssmtp/revaliases
